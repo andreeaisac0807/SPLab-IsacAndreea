6355c4ca0984d89c8ba3e209c812b4945e836600
Andreea <130660031+andreeaisac0807@users.noreply.github.com>
2023-10-28 16:59:59 +0300

    Tasks from 2nd lab

---
+++ Lab1/classes/Table.java
@@ -0,0 +1,15 @@
+public class Table {
+    private String tableTitle;
+   
+    public Table(String tableTitle) {
+        this.tableTitle = tableTitle;
+    }
+
+    public void setTitle(String tableTitle) {
+        this.tableTitle = tableTitle;
+    }
+
+    public String getTableTitle() {
+        return tableTitle;
+    }
+}
---
+++ Lab1/classes/TableOfContents.java
@@ -0,0 +1,17 @@
+class TabelOfContents {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public TabelOfContents(String text) {
+        this.text = text;
+    }
+
+    // Getters and setters
+}
