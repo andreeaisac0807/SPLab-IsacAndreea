29505a1ce3653623a1b80191eb35985f709e51a5
Andreea <130660031+andreeaisac0807@users.noreply.github.com>
2023-12-20 08:42:43 +0200

    second commit for lab 2

---
+++ src/main/java/ro/uvt/info/designpatternlab2023/DesignPatternLab2023Application.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.designpatternlab2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternLab2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternLab2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/ro/uvt/info/designpatternlab2023/DesignPatternLab2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.designpatternlab2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternLab2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
