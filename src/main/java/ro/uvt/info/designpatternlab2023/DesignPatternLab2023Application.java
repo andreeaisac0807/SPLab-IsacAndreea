b982ddc4b738b80eb0d2d084c28a62a590d89275
Andreea <130660031+andreeaisac0807@users.noreply.github.com>
2023-12-28 13:13:48 +0200

    test commit

--- src/main/java/ro/uvt/info/designpatternlab2023/DesignPatternLab2023Application.java
+++ src/main/java/ro/uvt/info/designpatternlab2023/DesignPatternLab2023Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesignPatternLab2023Application {
+    
 
     public static void main(String[] args) {
         SpringApplication.run(DesignPatternLab2023Application.class, args);
